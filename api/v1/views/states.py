4b3621a0e8d4f0eacc92fad125d2853523473669
monicajaimesc <monikmaja@gmail.com>
2020-01-28 23:41:50 +0000

    import added in the init, all the states are printed, need to write conditionals

---
+++ api/v1/views/states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+State objects that will handles all default RestFul API actions
+"""
+from api.v1.views import app_views
+from models import storage
+from flask import jsonify
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def view_states():
+    """
+   Resfult model (enpoints) that returns a list of states as arrays
+   type(State): <class 'type'>
+   properties:
+            type: String, of class object
+          created_at:
+            type: string, The date the object created
+          id:
+            type: string, the id of the state
+          name:
+            type: string, name of the state
+          updated_at:
+            type: string, date when the object was updated
+
+   """
+    states_ = storage.all('State')
+    state_return = []
+    for state in states_.values():
+        state_return.append(state.to_dict())
+    return jsonify(state_return)
