822409833fcc64b3e58ee2e498ab90c0a08e48a7
Dario Castaño <dario.castano@aim.com>
2020-01-28 08:58:34 -0500

    Set CORS to the api

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,11 +8,14 @@ from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 from flask import Flask
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+# Set CORS
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(err):
