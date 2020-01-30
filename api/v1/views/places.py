ed1187c2f70cffeeb84a00d9333a6e2d10adb4ec
monicajaimesc <monikmaja@gmail.com>
2020-01-30 02:00:21 +0000

    places and places_reviews added

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,7 +4,7 @@ Place objects that will handles all default RestFul API actions
 """
 from api.v1.views import app_views
 from models import storage
-from models.state import State
+from models.place import Place
 from flask import jsonify, abort, request, make_response
 from werkzeug.exceptions import BadRequest
 
@@ -15,11 +15,21 @@ def places_by_city(city_id):
     Handle places by city
     """
     if request.method == 'GET':
-        # TODO: Implement
-        pass
+        places_ = storage.all('Place')
+        place_return = []
+        for place in places_.values():
+            place_return.append(place.to_dict())
+        return jsonify(place_return)
     elif request.method == 'POST':
-        # TODO: Implement
-        pass
+        data = request.get_json()
+        if data is None:
+            abort(400, 'Not a JSON')
+        if data.get('name') is None:
+            abort(400, 'Missing name')
+        new_places = Place(**data)
+        new_places.save()
+        return make_response(jsonify(**new_places.to_dict()), 201)
+        
 
 
 @app_views.route('/places/<place_id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
@@ -27,12 +37,43 @@ def places(place_id):
     """
     Handle places in general
     """
+    place = storage.get('Place', place_id)
     if request.method == 'GET':
-        # TODO: Implement
-        pass
+        if place:
+            return jsonify(place.to_dict())
+        else:
+            abort(404, 'Not found')
     elif request.method == 'PUT':
-        # TODO: Implement
-        pass
+        changes = dict()
+
+        try:
+            changes = request.get_json()
+            if changes is None:
+                abort(400, 'Not a JSON')
+        except BadRequest:
+            abort(400, 'Not a JSON')
+            
+        target = storage.get('Place', place_id)
+        if target is None:
+            abort(404, 'Not found')
+
+        ignores = ('id', 'created_at', 'updated_at')
+
+        for key, val in changes.items():
+            if key in ignores:
+                pass
+            else:
+                setattr(target, key, val)
+
+        target.save()
+        return make_response(jsonify(**target.to_dict()), 200)
+            
     elif request.method == 'DELETE':
-        # TODO: Implement
-        pass
+        if place is None:
+            abort(404, 'Not found')
+        place = storage.get("Place", place_ids)
+        if place_ids is None:
+            abort(404, 'Not found')
+        storage.delete(place)
+        storage.save()
+        return jsonify({}), 200
--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -4,7 +4,7 @@ Review objects that will handles all default RestFul API actions
 """
 from api.v1.views import app_views
 from models import storage
-from models.state import State
+from models.review import Review
 from flask import jsonify, abort, request, make_response
 from werkzeug.exceptions import BadRequest
 
@@ -15,24 +15,63 @@ def reviews_by_place(place_id):
     Handle reviews by place
     """
     if request.method == 'GET':
-        # TODO: Implement
-        pass
+        review_ = storage.all('Review')
+        review_return = []
+        for review in review_.values():
+            review_return.append(review.to_dict())
+        return jsonify(review_return)
     elif request.method == 'POST':
-        # TODO: Implement
-        pass
-
+        data = request.get_json()
+        if data is None:
+            abort(400, 'Not a JSON')
+        if data.get('name') is None:
+            abort(400, 'Missing name')
+        new_review = Review(**data)
+        new_review.save()
+        return make_response(jsonify(**new_review.to_dict()), 201)
 
 @app_views.route('/reviews/<review_id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
 def reviews(review_id):
     """
     Handle reviews by id
     """
+    review = storage.get('Review', review_id)
     if request.method == 'GET':
-        # TODO: Implement
-        pass
+        if review:
+            return jsonify(review.to_dict())
+        else:
+            abort(404, 'Not found')
     elif request.method == 'PUT':
-        # TODO: Implement
-        pass
+        changes = dict()
+        
+        try:
+            changes = request.get_json()
+            if changes is None:
+                abort(400, 'Not a JSON')
+        except BadRequest:
+            abort(400, 'Not a JSON')
+
+        target = storage.get('Review', review_id)
+        if target is None:
+            abort(404, 'Not found')
+
+        ignores = ('id', 'created_at', 'updated_at')
+
+        for key, val in changes.items():
+            if key in ignores:
+                pass
+            else:
+                setattr(target, key, val)
+
+        target.save()
+        return make_response(jsonify(**target.to_dict()), 200)
+
     elif request.method == 'DELETE':
-        # TODO: Implement
-        pass
+        if review is None:
+            abort(404, 'Not found')
+        review = storage.get("Review", review_id)
+        if review_id is None:
+            abort(404, 'Not found')
+        storage.delete(review)
+        storage.save()
+        return jsonify({}), 200
