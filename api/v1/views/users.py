f37964758d1b57784e479e80a3b1be779fbc7c0f
Dario Castaño <dario.castano@aim.com>
2020-01-29 20:19:46 -0500

    Add Users CRUD

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,7 +4,7 @@ User objects that will handles all default RestFul API actions
 """
 from api.v1.views import app_views
 from models import storage
-from models.state import State
+from models.user import User
 from flask import jsonify, abort, request, make_response
 from werkzeug.exceptions import BadRequest
 
@@ -15,11 +15,22 @@ def users():
     Handle users in general
     """
     if request.method == 'GET':
-        # TODO: Implement
-        pass
+        user_list = storage.all('User')
+        user_return = []
+        for value in user_list.values():
+            user_return.append(value.to_dict())
+        return jsonify(user_return)
     elif request.method == 'POST':
-        # TODO: Implement
-        pass
+        data = request.get_json()
+        if data is None:
+            abort(400, 'Not a JSON')
+        if data.get('email') is None:
+            abort(400, 'Missing email')
+        if data.get('password') is None:
+            abort(400, 'Missing password')
+        new_user = User(**data)
+        new_user.save()
+        return make_response(jsonify(**new_user.to_dict()), 201)
 
 
 @app_views.route('/users/<user_id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
@@ -27,12 +38,35 @@ def user_by_id(user_id):
     """
     Handle user by id
     """
+    user = storage.get('Amenity', user_id)
+    if user is None:
+        abort(404, 'Not found')
+
     if request.method == 'GET':
-        # TODO: Implement
-        pass
+        return make_response(jsonify(user.to_dict()), 200)
+
     elif request.method == 'PUT':
-        # TODO: Implement
-        pass
+        changes = dict()
+
+        try:
+            changes = request.get_json()
+            if changes is None:
+                abort(400, 'Not a JSON')
+        except BadRequest:
+            abort(400, 'Not a JSON')
+
+        ignores = ('id', 'email',  'created_at', 'updated_at')
+
+        for key, val in changes.items():
+            if key in ignores:
+                pass
+            else:
+                setattr(user, key, val)
+
+        user.save()
+        return make_response(jsonify(**user.to_dict()), 200)
+
     elif request.method == 'DELETE':
-        # TODO: Implement
-        pass
+        storage.delete(user)
+        storage.save()
+        return make_response(jsonify({}), 200)
