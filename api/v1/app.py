3fbf02f5f87c3898505b99ec4e2389871d27505e
Dario Castaño <dario.castano@aim.com>
2020-01-30 17:46:34 -0500

    Fix CORS origin

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 # Set CORS
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 def send_json_error(err, code):
